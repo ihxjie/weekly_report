921bbb2e77703ef71d76dae5503afbc57b3f4514
ihxjie <xingjie1999@gmail.com>
2023-03-05 10:20:22 +0800

    feat: change davinci-003 to gpt-3.5-turbo

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -4,9 +4,16 @@ import {
   ReconnectInterval,
 } from "eventsource-parser";
 
+export type ChatGPTAgent = "user" | "system";
+
+export interface ChatGPTMessage {
+  role: ChatGPTAgent;
+  content: string;
+}
+
 export interface OpenAIStreamPayload {
   model: string;
-  prompt: string;
+  messages: ChatGPTMessage[];
   temperature: number;
   top_p: number;
   frequency_penalty: number;
@@ -34,22 +41,21 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
   const useUserKey = process.env.NEXT_PUBLIC_USE_USER_KEY === "true" ? true : false;
 
   var openai_api_key = (useUserKey ? payload.api_key : process.env.OPENAI_API_KEY) || ""
-  openai_api_key = newapikey
-  console.log(openai_api_key)
-  console.log(111222)
+  if(!useUserKey){
+    openai_api_key = newapikey
+  }
 
   function checkString(str :string) {
     var pattern = /^sk-[A-Za-z0-9]{48}$/;
     return pattern.test(str);
   }
   if(!checkString(openai_api_key)) {
-    console.log(openai_api_key)
     throw new Error('OpenAI API Key Format Error')
   }
 
   delete payload.api_key
 
-  const res = await fetch("https://api.openai.com/v1/completions", {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${openai_api_key ?? ""}`,
@@ -71,7 +77,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
           }
           try {
             const json = JSON.parse(data);
-            const text = json.choices[0].text;
+            const text = json.choices[0].delta?.content || "";
             if (counter < 2 && (text.match(/\n/) || []).length) {
               // this is a prefix character (i.e., "\n\n"), do nothing
               return;
